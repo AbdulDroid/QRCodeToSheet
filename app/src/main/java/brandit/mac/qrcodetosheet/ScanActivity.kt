e0401b7190b500c15aa8e1934e86ae8e4b9f2b55
Abdulrahman Abdulkarim <olanre12waju@gmail.com>
2018-06-12 11:28:20 +0100

    Refactor App Script URL

--- app/src/main/java/brandit/mac/qrcodetosheet/ScanActivity.kt
+++ app/src/main/java/brandit/mac/qrcodetosheet/ScanActivity.kt
@@ -11,7 +11,6 @@ import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_scan.*
 import org.json.JSONObject
-import pub.devrel.easypermissions.EasyPermissions
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
@@ -26,7 +25,7 @@ import javax.net.ssl.HttpsURLConnection
 
 
 
-class ScanActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+class ScanActivity : AppCompatActivity() {
 
     lateinit var scannedData: String
     private var mToast: Toast? = null
@@ -49,54 +48,6 @@ class ScanActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
         })
     }
 
-
-//    private fun updateSpreadSheet() {
-//        val f_name: String = name.text.toString()
-//        val name_size: Int = f_name.split(" ").size
-//        val u_email: String = email.text.toString()
-//        val gen: String = gender.text.toString()
-//        val u_age: String = age.text.toString()
-//        val u_height: String = height.text.toString()
-//        val u_origin: String = origin.text.toString()
-//
-//        if (TextUtils.isEmpty(f_name) || name_size < 2) {
-//            name.error = ("Please enter your full name")
-//            name.requestFocus()
-//        } else if (TextUtils.isEmpty(u_email) || !Patterns.EMAIL_ADDRESS.matcher(u_email).matches()) {
-//            email.error = ("Please enter a valid email address")
-//            email.requestFocus()
-//        } else if (TextUtils.isEmpty(gen)) {
-//            gender.error = ("Please enter your gender")
-//            gender.requestFocus()
-//        } else if (TextUtils.isEmpty(u_age) || u_age.toInt() < 13) {
-//            age.error = ("Please enter age above 13 to continue")
-//            age.requestFocus()
-//        } else if (TextUtils.isEmpty(u_height)) {
-//            height.error = ("Please enter your height in cm")
-//            height.requestFocus()
-//        } else if (TextUtils.isEmpty(u_origin)) {
-//            origin.error = ("Please enter your state of origin")
-//            origin.requestFocus()
-//        } else {
-//            progress_bar.visibility = View.VISIBLE
-//            apiService.response(f_name, u_email, gen, u_age, u_height, u_origin)
-//                    .enqueue(object : Callback<ResponseBody> {
-//                        override fun onResponse(@NonNull call: Call<ResponseBody>?, @NonNull response: Response<ResponseBody>?) {
-//                            if (response!!.isSuccessful)
-//                                onSheetUpdated(response.body().toString())
-//                            else
-//                                onError(response.errorBody()!!.string())
-//
-//                            Log.e("Response", "" + response.code() + ": " + response.toString())
-//                        }
-//
-//                        override fun onFailure(@NonNull call: Call<ResponseBody>?, @NonNull t: Throwable?) {
-//                            Log.e("Response", t!!.message, t)
-//                        }
-//                    })
-//        }
-//    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null && result.contents != null) {
@@ -117,7 +68,7 @@ class ScanActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
             try {
                 //Enter script URL Here
-                val url = URL("https://script.google.com/macros/s/AKfycbwA4GnUWHQwvRX635hI009yPaTeS5svwsn3FHPxMSLoGb4XY3g/dev")
+                val url = URL("https://script.google.com/macros/s/AKfycbz1vJystFs-4p2YTVgt7vOv9g99ll-uHoHvdbIG_qgI00LhCBI/exec")
 
                 val postDataParams = JSONObject()
 
@@ -129,12 +80,7 @@ class ScanActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
                 //Passing scanned code as parameter
 
-                postDataParams.put("sdata", scannedData)/*
-                postDataParams.put("email", email.text.toString())
-                postDataParams.put("gender", gender.text.toString())
-                postDataParams.put("age", age.text.toString())
-                postDataParams.put("height", height.text.toString())
-                postDataParams.put("origin", origin.text.toString())*/
+                postDataParams.put("sdata", scannedData)
 
 
                 Log.e("params", postDataParams.toString())
@@ -207,29 +153,10 @@ class ScanActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
             result.append(URLEncoder.encode(key, "UTF-8"))
             result.append("=")
-            result.append(URLEncoder.encode(value.toString(), "UTF-8"))
+            result.append(URLEncoder.encode(value, "UTF-8"))
 
         }
         return result.toString()
     }
 
-    /*private fun initRetrofit() {
-        val gson: Gson = GsonBuilder()
-                .setLenient()
-                .create()
-        val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
-
-        val netInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-
-        val retrofit: Retrofit = Retrofit.Builder()
-                .baseUrl(ApiService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .client(httpClient
-                        .addNetworkInterceptor(netInterceptor)
-                        .connectTimeout(100, TimeUnit.SECONDS)
-                        .readTimeout(100, TimeUnit.SECONDS).build())
-                .build()
-
-        apiService = retrofit.create(ApiService::class.java)
-    }*/
 }
